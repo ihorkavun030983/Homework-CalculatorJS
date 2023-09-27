bd35c7d7f9a0602cfe268f6a255bdccce74d1fe9
fantom030983 <ihorkavun1234@gmail.com>
2023-09-27 14:23:49 +0300

    JS Calculator

---
+++ js/calculator.js
@@ -0,0 +1,10 @@
+let userName = prompt('Яку дію ти хочеш зробити?');
+
+var a = prompt('Введіть перше число', ' ');
+var b = prompt('Введіть друге число', ' ');
+
+console.log(Number(a) + Number(b));
+console.log(Number(a) - Number(b));
+console.log(Number(a) * Number(b));
+console.log(Number(a) / Number(b));
+    
